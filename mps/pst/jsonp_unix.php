357771cbf0b44b7843b908867fe894bb5af4e313
Miko <miko645@gmx.com>
2020-06-19 20:47:29 -0700

    initial commit

---
+++ mps/pst/jsonp_unix.php
@@ -0,0 +1,7 @@
+(function(gwtpst){
+	if(typeof gwtpst === "undefined" )
+		return false;
+	gwtpst._jsonpResponseProcess(function(){
+		return {time: 1592150314, timezone: -480}
+	});
+})(gwtPstWidget);
