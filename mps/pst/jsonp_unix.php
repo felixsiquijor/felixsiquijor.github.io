d26e3969e13e29a0473558435a6cd90104728a54
felixsiquijor <felix_siquijor@hotmail.com>
2020-06-23 19:30:09 -0700

    removed mps folder

--- mps/pst/jsonp_unix.php
+++
@@ -1,7 +0,0 @@
-(function(gwtpst){
-	if(typeof gwtpst === "undefined" )
-		return false;
-	gwtpst._jsonpResponseProcess(function(){
-		return {time: 1592150314, timezone: -480}
-	});
-})(gwtPstWidget);
